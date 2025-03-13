c636d5e8e95cf475c4339b8013a0c5a8dc4cb11d
Ranvitha <ranvithaparshi07@gmail.com>
2025-03-13 16:30:15 +0530

    tranformation of data is done

--- src/logger.py
+++ src/logger.py
@@ -1,3 +1,4 @@
+
 import logging 
 
 import os
@@ -15,3 +16,27 @@ logging.basicConfig(
     level=logging.INFO,
 
 )
+
+
+
+# import logging
+# import os
+# from datetime import datetime
+
+
+# logs_dir = os.path.join(os.getcwd(), "logs")
+# os.makedirs(logs_dir, exist_ok=True)  
+
+
+# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# logging.basicConfig(
+#     filename=LOG_FILE_PATH,
+#     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+#     level=logging.INFO,
+#     handlers=[
+#         logging.FileHandler(LOG_FILE_PATH),  
+#         logging.StreamHandler()  
+#     ]
+# )
